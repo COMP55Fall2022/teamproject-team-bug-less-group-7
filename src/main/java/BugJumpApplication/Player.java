298fd4643bdc8056d6435db9353cbca18fc95727
Corwin Hsien <chsien5@gmail.com>
2022-10-31 16:52:33 -0700

    remove void from player constructor

--- src/main/java/BugJumpApplication/Player.java
+++ src/main/java/BugJumpApplication/Player.java
@@ -10,7 +10,7 @@ public class Player {
 	
 	//Constructor for player.java that defaults the variables for the player and sets the x and y
 	//to the inputed values.
-	public void Player(int x,int y) {
+	public Player(int x,int y) {
 		//TODO: instantiate weapon
 		xAxis = x;
 		yAxis = y;
