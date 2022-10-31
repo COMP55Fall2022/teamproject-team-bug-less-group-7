f5b15dad356487b6fa2517a245c4418645aaf9b5
MichaelJr1 <m_lewis18@u.pacific.edu>
2022-10-31 16:42:43 -0700

    Added GetY

--- src/main/java/BugJumpApplication/Player.java
+++ src/main/java/BugJumpApplication/Player.java
@@ -6,9 +6,14 @@ public class Player {
 	int xAxis;
 	boolean isRightOrientation;
 	boolean isJumping;
+	int y;
 	
 	
 	public void Player(int x,int y) {
+			 
+		 }
+	public int getY() {
+		return this.y;
 		
 	}
 	
