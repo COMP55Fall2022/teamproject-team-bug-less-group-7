c8e4d16a21b5495b2b50687f7867968479c68b19
Corwin Hsien <chsien5@gmail.com>
2022-10-31 16:36:27 -0700

    Created Player.java constructor

--- src/main/java/BugJumpApplication/Player.java
+++ src/main/java/BugJumpApplication/Player.java
@@ -2,14 +2,20 @@ package BugJumpApplication;
 
 
 public class Player {
+	//TODO: Needs weapon
 	int yAxis;
 	int xAxis;
 	boolean isRightOrientation;
 	boolean isJumping;
 	
-	
+	//Constructor for player.java that defaults the variables for the player and sets the x and y
+	//to the inputed values.
 	public void Player(int x,int y) {
-		
+		//TODO: instantiate weapon
+		xAxis = x;
+		yAxis = y;
+		isRightOrientation = true;
+		isJumping = false;
 	}
 	
 }
