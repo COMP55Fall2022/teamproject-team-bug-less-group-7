6ebffb9643bdd2fd0a25a389e127637aa5975914
Jacob Bejarano <jacobjbejarano@gmail.com>
2022-10-31 16:50:19 -0700

    fixed Player

--- src/main/java/BugJumpApplication/Player.java
+++ src/main/java/BugJumpApplication/Player.java
@@ -2,18 +2,35 @@ package BugJumpApplication;
 
 
 public class Player {
+	//TODO: Needs weapon
 	int yAxis;
 	int xAxis;
 	boolean isRightOrientation;
 	boolean isJumping;
 	
-	
-	public Player(int x,int y) {
-			 
-		 }
-	public int getY() {
-		return this.yAxis;
-		
+	//Constructor for player.java that defaults the variables for the player and sets the x and y
+	//to the inputed values.
+	public void Player(int x,int y) {
+		//TODO: instantiate weapon
+		xAxis = x;
+		yAxis = y;
+		isRightOrientation = true;
+		isJumping = false;
 	}
+	
+	public void move(int dx, int dy) {
+		 if(dx < 0) {
+			 isRightOrientation = false;
+		 }
+		 else {
+			isRightOrientation = true;
+		}
+		 this.xAxis += dx;
+		 this.yAxis += dy;
+	 }
+	 
+	 public boolean getIsJumping() {
+		 return isJumping;
+	 }
 	
 }
