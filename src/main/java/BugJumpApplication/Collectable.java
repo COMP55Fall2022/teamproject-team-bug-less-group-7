c80631de64803f4d1e3c2488c0365304994a55f0
Osbaldo <o_ramirez3@u.pacific.edu>
2022-11-04 16:58:22 -0700

    Finished methods and variables for collectable class

--- src/main/java/BugJumpApplication/Collectable.java
+++ src/main/java/BugJumpApplication/Collectable.java
@@ -1,6 +1,27 @@
 package BugJumpApplication;
 
 public class Collectable {
+	int xAxis;
+	int yAxis;
+	CollectableType cType;
 	
+	public Collectable(int x, int y, CollectableType cType) {
+		this.cType = cType;
+		this.xAxis = x;
+		this.yAxis = y;
+	}
+	
+	public CollectableType getCType() {
+		return cType;
+	}
+	
+	public int getY() {
+		return this.yAxis;
+	}
+	
+	public int getX() {
+		return this.xAxis;
+	}
+
 	
 }
