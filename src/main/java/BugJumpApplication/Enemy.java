8a647b35251250b0322d9b0cf59592ef6e6cec19
Charlotte Oicles <c_oicles@u.pacific.edu>
2022-11-04 16:26:23 -0700

    Completed enemy classes and enum

--- src/main/java/BugJumpApplication/Enemy.java
+++ src/main/java/BugJumpApplication/Enemy.java
@@ -1,5 +1,25 @@
 package BugJumpApplication;
 
-public class Enemy {
+import javax.swing.Timer;
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
+public class Enemy {
+	
+	
+	Timer time;
+	int width;
+	int height;
+	int xAxis;
+	int yAxis;
+	boolean isRightOrientation;
+	boolean willAttack;
+	EnemyType type;
+	
+	public Enemy(EnemyType eType, int x, int y) {
+		this.type = eType;
+		xAxis = x;
+		yAxis = y;
+		isRightOrientation = true;
+	}
 }
--- src/main/java/BugJumpApplication/EnemyType.java
+++ src/main/java/BugJumpApplication/EnemyType.java
@@ -1,5 +1,18 @@
 package BugJumpApplication;
 
 public enum EnemyType {
-
+	FLOWER,
+	SPIDER,
+	WORM,
+	BEATLE;
+	
+	public String toString() {
+		switch(this) {
+			case FLOWER: return "flower";
+			case SPIDER: return "spider";
+			case WORM: return "worm";
+			case BEATLE: return "beatle";
+		}
+		return "n/a";
+	}
 }
