df43a216f2cc6221d81987e49cc4f9e6cb5339a6
MichaelJr1 <m_lewis18@u.pacific.edu>
2022-11-04 16:27:40 -0700

    TerrainType

---
+++ src/main/java/BugJumpApplication/TerrainType.java
@@ -0,0 +1,16 @@
+package BugJumpApplication;
+
+public enum TerrainType {
+	SPIKE, GRASS, DIRT,;
+	int num;
+	public TerrainType getType() {
+		switch(num){
+			case 0: return TerrainType.SPIKE;
+			case 1: return TerrainType.GRASS;
+			case 2: return TerrainType.DIRT;
+		}
+	;
+	return null;
+		
+	}
+}
