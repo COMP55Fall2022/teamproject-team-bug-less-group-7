2b21dfbf1424645b12ca2865701f6ae151a7b122
Osbaldo <o_ramirez3@u.pacific.edu>
2022-11-04 16:22:32 -0700

    Created tType,eType, and getEnemyType, and constructor class

--- src/main/java/BugJumpApplication/Terrain.java
+++ src/main/java/BugJumpApplication/Terrain.java
@@ -6,8 +6,21 @@ public class Terrain {
 	private int xAxis;
 	private int yAxis;
 	
-	Terrain tType;
+//	TerrainType tType;
+	EnemyType eType;
+		
+//	private void Terrain(int x, int y, int width, int height, TerrainType tType) {
+
+//		this.yAxis = y;
+//		this.xAxis = x;
+//		this.height = height;
+//		this.width = width;
+	//	this.tType = tType;
+//	}
 	
+	public EnemyType getEnemyType() {
+		return eType;
+	}
 	
 	public int getY() {
 		return this.yAxis;
