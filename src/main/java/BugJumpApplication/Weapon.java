cbc66432c4b5ef1d59fbad785bd0a67152cf9157
Osbaldo <o_ramirez3@u.pacific.edu>
2022-11-09 16:40:37 -0800

    Created weapon class along with methods and constructor. Attack method commented out for now

---
+++ src/main/java/BugJumpApplication/Weapon.java
@@ -0,0 +1,19 @@
+package BugJumpApplication;
+
+public class Weapon {
+
+	WeaponType wType;
+	
+	public Weapon(WeaponType wType){
+		
+	}
+	
+	public WeaponType getWeaponType() {
+		return wType;
+	}
+	
+//	public Bullet attack() {
+//		
+//	}
+	
+}
