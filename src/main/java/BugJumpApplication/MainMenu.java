40191978757d8c1795be4e7c75288094dacc576e
MichaelJr1 <m_lewis18@u.pacific.edu>
2022-11-21 20:47:32 -0800

    Implemented Main Menu

--- src/main/java/BugJumpApplication/MainMenu.java
+++ src/main/java/BugJumpApplication/MainMenu.java
@@ -1,8 +1,12 @@
 package BugJumpApplication;
 
+import java.awt.Color;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.program.Program;
@@ -30,6 +34,13 @@ public class MainMenu extends GraphicsPane {
 		super();
 		program = e;
 	}
+	GLabel title = new GLabel("Bug Jump", 400, 200);
+	GRect play = new GRect (200, 270, 600, 100);
+	GLabel playButton = new GLabel("Play", 470, 330);
+	GRect tutorial = new GRect(200, 430, 600, 100);
+	GLabel tutorialButton = new GLabel("Tutorial", 430, 490);
+	GRect quit = new GRect(200, 590, 600, 100);
+	GLabel quitButton = new GLabel("Quit", 470, 650);
 
 @Override
 	public void mousePressed(MouseEvent e) {
@@ -39,10 +50,47 @@ public class MainMenu extends GraphicsPane {
 
 	@Override
 	public void showContents() {
-		program.add(new GRect(200, 200, 200, 200));
+		//GRect rect = new GRect(500, 300, 100, 100);
+		//rect.setFillColor(Color.GREEN);
+		//rect.setFilled(true);
+		//add(rect);
+		
+		program.add(title);
+		title.setFont("Arial-Bold-60");
+		
+		program.add(play);		
+		program.add(playButton);
+		playButton.setFont("Arial-Bold-40");
+		
+		program.add(tutorial);
+		program.add(tutorialButton);
+		tutorialButton.setFont("Arial-Bold-40");
+		
+		program.add(quit);
+		program.add(quitButton);
+		quitButton.setFont("Arial-Bold-40");
+		
+		
+		//program.add(new GRect(500, 300, 100, 100));
+		//program.add(new GLabel("Bug Jump",500, 200));
+		
+		
+		
+		
+	
+		
 	}
 	
 	
+	
+
+
+
+	private void add(GRect rect) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public void hideContents() {
 		// TODO Auto-generated method stub
