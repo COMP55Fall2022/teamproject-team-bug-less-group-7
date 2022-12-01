bceb624e741438d2957809e77d415dee6facbfd0
Corwin Hsien <chsien5@gmail.com>
2022-11-30 16:11:09 -0800

    Started Work on level 3

--- src/main/java/BugJumpApplication/MainGame.java
+++ src/main/java/BugJumpApplication/MainGame.java
@@ -83,7 +83,7 @@ public class MainGame extends GraphicsPane {
 	public MainGame(MainApplication e, int level) {
 		program = e;
 		this.level = level;
-		//level = x;
+		level = 3;
 	}
 	
 	@Override
